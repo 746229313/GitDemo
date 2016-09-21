828852082ce59442405473c50d6a304b3afeedb2
lucktoyou <1426209678@qq.com>
2016-09-21 17:07:15 +0800

    ss

--- app/src/main/java/com/liguanjia/gitdemo/MainActivity.java
+++ app/src/main/java/com/liguanjia/gitdemo/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //再难的东西，学会就建党了。
     }
 }
